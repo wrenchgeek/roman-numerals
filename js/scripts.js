f0d255498751337c8b12cff31b661baaaf0a4c33
Jim Patt & franz knupfer <student@epicodus.com>
2015-05-28 10:25:20 -0700

    convert the number 4 into roman numeral and add a test to convert the number 8

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 var roman = function(number) {
   var numeral1 = "I";
+  var numeral5 = "V";
   var outputArray = [];
-  if (number <= 3) {
-    for (var index = 1; index <= number; index +=1) {
+  var remainder = number
+
+  if ((remainder / 5 < 1) && (remainder % 5 === 4)) {
+    outputArray.push(numeral1, numeral5);
+    remainder -=4
+  }
+
+
+
+  if (remainder <= 3) {
+    for (var index = 1; index <= remainder; index +=1) {
       outputArray.push(numeral1);
     }
-    var finalOutput = outputArray.join('');
+
   }
+  var finalOutput = outputArray.join('');
   return finalOutput;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,13 @@ describe("roman", function() {
   it("takes a number less than 4 and converts it to a roman numeral", function() {
     expect(roman(3)).to.eql("III");
   });
+
+  it("takes the number 4 and converts it to a roman numeral", function() {
+    expect(roman(4)).to.eql("IV");
+  });
+
+  it("takes the number 8 and converts it to a roman numeral", function() {
+    expect(roman(8)).to.eql("VIII");
+  });
+
 });
