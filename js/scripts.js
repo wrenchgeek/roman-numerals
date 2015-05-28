661d8a124888b538f7402cac2273e53520309d42
Jim Patt & franz knupfer <student@epicodus.com>
2015-05-28 11:30:37 -0700

    roman now works up to 89

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,51 @@
 var roman = function(number) {
   var numeral1 = "I";
   var numeral5 = "V";
+  var numeral10 = "X";
+  var numeral50 = "L";
   var outputArray = [];
-  var remainder = number
+  var remainder = number;
+
+  if (remainder / 50 > 1) {
+    outputArray.push(numeral50);
+    remainder -= 50;
+  }
+
+  if ((remainder / 50 < 1) && (40 <= remainder % 50) && (remainder % 50 <= 49)) {
+    outputArray.push(numeral10, numeral50);
+    remainder -=40;
+  }
+
+  if (remainder / 10 > 1) {
+    var tens = Math.floor(remainder / 10);
+    for (var index = 1; index <= tens; index += 1) {
+      outputArray.push(numeral10);
+      remainder -= 10;
+    }
+  }
+
+  if ((remainder / 10 < 1) && (remainder % 10 === 9)) {
+    outputArray.push(numeral1, numeral10);
+    remainder -= 10;
+  }
 
   if (remainder / 5 > 1) {
     outputArray.push(numeral5);
     remainder -= 5;
-
   }
 
   if ((remainder / 5 < 1) && (remainder % 5 === 4)) {
     outputArray.push(numeral1, numeral5);
-    remainder -=4
+    remainder -=5;
   }
-
 
 
   if (remainder <= 3) {
     for (var index = 1; index <= remainder; index +=1) {
       outputArray.push(numeral1);
     }
-
   }
+
   var finalOutput = outputArray.join('');
   return finalOutput;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -10,8 +10,32 @@ describe("roman", function() {
   it("takes the number 8 and converts it to a roman numeral", function() {
     expect(roman(8)).to.eql("VIII");
   });
+
   it("takes the number 9 and converts it to a roman numeral", function() {
     expect(roman(9)).to.eql("IX");
   });
 
+  it("takes the number 13 and converts it to a roman numeral", function() {
+    expect(roman(13)).to.eql("XIII");
+  });
+
+  it("takes the number 14 and converts it to a roman numeral", function() {
+    expect(roman(14)).to.eql("XIV");
+  });
+
+  it("takes the number 39 and converts it to a roman numeral", function() {
+    expect(roman(39)).to.eql("XXXIX");
+  });
+
+  it("takes the number 48 and converts it to a roman numeral", function() {
+    expect(roman(48)).to.eql("XLVIII");
+  });
+
+  it("takes the number 53 and converts it to a roman numeral", function() {
+    expect(roman(53)).to.eql("LIII");
+  });
+
+  it("takes the number 89 and converts it to a roman numeral", function() {
+    expect(roman(89)).to.eql("LXXXIX");
+  });
 });
